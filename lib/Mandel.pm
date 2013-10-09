c9206e476775e9d51155db2c598ce82563f3641e
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-10-09 21:44:53 +0200

    Released version 0.08
           * Add support for setting belongs_to accessor to oid
           * Fix Model->name() was not set when built from documents on disk

--- lib/Mandel.pm
+++ lib/Mandel.pm
@@ -6,7 +6,7 @@ Mandel - Async model layer for MongoDB objects using Mango
 
 =head1 VERSION
 
-0.07
+0.08
 
 =head1 SYNOPSIS
 
@@ -97,7 +97,7 @@ use Mandel::Model;
 use Mango;
 use Carp 'confess';
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 my $LOADER = Mojo::Loader->new;
 
