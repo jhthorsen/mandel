ae028d882f7ed59b93d2edddb40dce1c844fe614
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-04-26 18:28:58 +0200

    Released version 0.19
           * Fix DBRef in database: "$ref" did not have the right collection name

--- lib/Mandel.pm
+++ lib/Mandel.pm
@@ -6,7 +6,7 @@ Mandel - Async model layer for MongoDB objects using Mango
 
 =head1 VERSION
 
-0.18
+0.19
 
 =head1 SYNOPSIS
 
@@ -104,7 +104,7 @@ use Mandel::Model;
 use Mango;
 use Carp 'confess';
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 
 my $LOADER = Mojo::Loader->new;
 
