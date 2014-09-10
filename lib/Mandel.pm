a53542cfb641fecb48758db3c9593152c30fed0c
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-09-10 15:51:11 +0200

    Released version 0.21
           * Fix document extends
             Contributor: Alexey Neyasov

--- lib/Mandel.pm
+++ lib/Mandel.pm
@@ -6,7 +6,7 @@ Mandel - Async model layer for MongoDB objects using Mango
 
 =head1 VERSION
 
-0.20
+0.21
 
 =head1 SYNOPSIS
 
@@ -104,7 +104,7 @@ use Mandel::Model;
 use Mango;
 use Carp 'confess';
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 my $LOADER = Mojo::Loader->new;
 
