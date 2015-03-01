9e191926859913fdc4f495351c520e5cde865c87
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-03-01 17:52:49 +0100

    Released version 0.24
           * Compatible with Mojolicious 6.0

--- lib/Mandel.pm
+++ lib/Mandel.pm
@@ -6,7 +6,7 @@ Mandel - Async model layer for MongoDB objects using Mango
 
 =head1 VERSION
 
-0.23
+0.24
 
 =head1 SYNOPSIS
 
@@ -104,7 +104,7 @@ use Mandel::Model;
 use Mango;
 use Carp 'confess';
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 =head1 ATTRIBUTES
 
