4f4b7564359a350dffbe0918936bb353dae92914
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-01-07 10:05:25 +0100

    Released version 0.29
     - Fix all_document_names() will skip non-document classes

--- lib/Mandel.pm
+++ lib/Mandel.pm
@@ -6,7 +6,7 @@ Mandel - Async model layer for MongoDB objects using Mango
 
 =head1 VERSION
 
-0.28
+0.29
 
 =head1 SYNOPSIS
 
@@ -104,7 +104,7 @@ use Mandel::Model;
 use Mango;
 use Carp 'confess';
 
-our $VERSION = '0.28';
+our $VERSION = '0.29';
 
 =head1 ATTRIBUTES
 
